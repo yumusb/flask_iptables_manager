e3cff022361dad730ced6b0fa874b79501944c7f
榆木 <43062104+yumusb@users.noreply.github.com>
2021-04-17 21:19:01 +0800

    Update flask_iptables_manager.py

--- flask_iptables_manager.py
+++ flask_iptables_manager.py
@@ -68,7 +68,7 @@ def DelAllIpRules():
 @app.route(FLASK_PATH+"/admin/add/",methods=['POST'])
 def admin_add():
     param = request.form['p']
-    params = re.sub('[^\d\.\/]+',',',params)
+    param = re.sub('[^\d\.\/]+',',',param)
     params = param.split(",")
     base_commands = []
     pattern = re.compile(r'^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([1-9]|[1-2]\d|3[0-2])){0,1}$')
